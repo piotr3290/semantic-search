104870572c7e1f1d846bfd78207e7334ab251c3e
piotr3290 <piotr3290@wp.pl>
2025-09-09 21:30:20 +0200

    KeywordsExtractor changes

--- app/KeywordsExtractor.py
+++ app/KeywordsExtractor.py
@@ -2,10 +2,11 @@ from keybert import KeyBERT
 
 
 class KeywordsExtractor:
-    def __init__(self):
-        self.kw_model = KeyBERT()
+    def __init__(self, top_n:int, min_threshold:float, model):
+        self.kw_model = KeyBERT(model=model)
+        self.top_n = top_n
+        self.min_threshold = min_threshold
 
-    def get_keywords(self, search_phrase):
-        keywords = self.kw_model.extract_keywords(search_phrase)
-        print(f"Selected Keywords: {keywords}")
-        return [keyword for keyword, _ in keywords]
+    def get_keywords(self, search_phrase:str)->list:
+        keywords = self.kw_model.extract_keywords(search_phrase, top_n=self.top_n, use_mmr=True)
+        return [keyword for keyword, rate in keywords if rate >= self.min_threshold]
